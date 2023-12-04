a1ee835f7bf55a57b68148903e26551c9ca3ea7e
VasylPopaz <pafos1997@gmail.com>
2023-12-04 10:17:14 +0100

    upd task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,44 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 let formData = {};
+// Save to local storage
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+// Load from local storage
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
 
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  // localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  save('feedback-form-state', formData);
 }
 
 function onPageLoad() {
-  const savedKey = localStorage.getItem('feedback-form-state');
+  // const savedKey = localStorage.getItem('feedback-form-state');
+  // if (!savedKey) return;
+  // try {
+  //   formData = JSON.parse(savedKey);
+  // } catch (error) {
+  //   console.log(error.name);
+  //   console.log(error.message);
+  // }
+
+  const savedKey = load('feedback-form-state');
   if (!savedKey) return;
-  try {
-    formData = JSON.parse(savedKey);
-  } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-  }
+  formData = savedKey;
   Object.keys(formData).forEach(key => {
     form[key].value = formData[key];
   });
