ba5f5a3d1d72191d2af3ae31d360d3c165bd1850
VasylPopaz <pafos1997@gmail.com>
2023-12-04 15:11:39 +0100

    upd task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,63 +1,34 @@
 import throttle from 'lodash.throttle';
+import storage from './storage';
 
 const form = document.querySelector('.feedback-form');
 let formData = {};
-
-document.addEventListener('DOMContentLoaded', onPageLoad);
+onPageLoad();
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-// Save to local storage
-function saveToLS(key, value) {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error('Set state error: ', error.message);
-  }
-}
-// Load from local storage
-function loadToLS(key) {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-}
-
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
-  // localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  saveToLS('feedback-form-state', formData);
+  storage.saveToLS('feedback-form-state', formData);
 }
 
 function onPageLoad() {
-  // const savedKey = localStorage.getItem('feedback-form-state');
-  // if (!savedKey) return;
-  // try {
-  //   formData = JSON.parse(savedKey);
-  // } catch (error) {
-  //   console.log(error.name);
-  //   console.log(error.message);
-  // }
-
-  const savedKey = loadToLS('feedback-form-state');
-  if (!savedKey) return;
-  formData = savedKey;
+  const savedKey = storage.loadToLS('feedback-form-state');
+  formData = savedKey || {};
   Object.keys(formData).forEach(key => {
     form[key].value = formData[key];
   });
 }
 
 function onFormSubmit(event) {
+  const { email, message } = event.target;
   event.preventDefault();
-  if (!form['email'].value || !form['message'].value) {
+  if (!email.value || !message.value) {
     alert('Введіть данні!');
     return;
   }
   console.log(formData);
   formData = {};
   localStorage.removeItem('feedback-form-state');
-  form.reset();
+  event.target.reset();
 }
---
+++ src/js/storage.js
@@ -0,0 +1,23 @@
+// Save to local storage
+function saveToLS(key, value) {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+// Load from local storage
+function loadToLS(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+export default {
+  saveToLS,
+  loadToLS,
+};
